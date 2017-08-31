47d9e5a3fc58f1abb71339a1f1b1a3ca2ff05030
John Ihnotic <jihnotic@knuedge.com>
2017-08-31 15:42:41 -0700

    Added sorting by card cost

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 Python command line script to print out decklist based from deckid string input
 ## Requirements
 
-* Python 2.7 or greater
+* Python 2.7 or Python 3.4
 * hearthstone python library (https://github.com/HearthSim/python-hearthstone)
-`pip install hearthstone`
+* `pip install hearthstone`
 
 ## Usage
 
@@ -13,28 +13,27 @@ From command line: `./hs-decoder.py <deckstring>`
 ## Example
 ```
 $ ./hs-decoder.py AAECAa0GBgm0A6irAoW4Are7ApDTAgwelwKhBMkG0wrXCrW7Auq/AtHBAuXMArTOAvDPAgA=
-
 PRIEST
-FormatType.FT_STANDARD
-===========================
+FormatType.FT_STANDARD 
+==============================
+2x (1) Holy Smite
+2x (1) Potion of Madness
+2x (2) Mind Blast
+2x (2) Shadow Visions
+2x (2) Shadow Word: Pain
+2x (2) Spirit Lash
+2x (3) Shadow Word: Death
+2x (3) Thoughtsteal
+1x (4) Barnes
+2x (4) Eternal Servitude
+1x (4) Greater Healing Potion
+2x (5) Holy Nova
+2x (6) Dragonfire Potion
+2x (6) Shadow Essence
 1x (7) Prophet Velen
+1x (8) Shadowreaper Anduin
 1x (9) Malygos
 1x (10) Y'Shaarj, Rage Unbound
-1x (4) Barnes
-1x (4) Greater Healing Potion
-1x (8) Shadowreaper Anduin
-2x (3) Thoughtsteal
-2x (1) Holy Smite
-2x (2) Mind Blast
-2x (5) Holy Nova
-2x (3) Shadow Word: Death
-2x (2) Shadow Word: Pain
-2x (1) Potion of Madness
-2x (6) Dragonfire Potion
-2x (2) Shadow Visions
-2x (4) Eternal Servitude
-2x (6) Shadow Essence
-2x (2) Spirit Lash
-============================
+==============================
 Deckstring: AAECAa0GBgm0A6irAoW4Are7ApDTAgwelwKhBMkG0wrXCrW7Auq/AtHBAuXMArTOAvDPAgA=
 ```
--- hs-decoder.py
+++ hs-decoder.py
@@ -3,12 +3,12 @@ import os
 import json
 import requests
 import sys
-import requests
 from hearthstone.deckstrings import Deck
 
-deckstring = sys.argv[1]
 db_url = "https://api.hearthstonejson.com/v1/20457/enUS/cards.collectible.json"
 db_path = "./db.json"
+deckstring = sys.argv[1]
+deck = Deck.from_deckstring(deckstring)
 lb = "="*30
 if os.path.exists(db_path):
     with open(db_path, "r") as f:
@@ -16,21 +16,25 @@ if os.path.exists(db_path):
 else:
     response = requests.get(db_url)
     if response.status_code == 200:
-        with open(db_path, "w") as f:
+        with open(db_path, "wb") as f:
             f.write(response.text.encode("utf-8"))
             db = response.json()
     else:
         raise RuntimeError("Couldn't download cards database: %s"
                            % response.text)
-
 def get_card(dbf_id):
-    return next(card for card in db if card["dbfId"] == dbf_id)
-def format_card(card):
-    return "(%d) %s" % (card["cost"], card["name"])
-deck = Deck.from_deckstring(deckstring)
+    return next((card for card in db if card["dbfId"] == dbf_id), None)
+def sort_cards():
+    cards = []
+    for s in deck.cards:
+        card = get_card(s[0])
+        card["ammount"] = int(s[1])
+        cards.append(card)
+    cards = sorted(cards, key=lambda card:(card["cost"], card["name"]))
+    return cards
+
 print (get_card(deck.heroes[0])["cardClass"])
 print ("%s \n%s" % (deck.format, lb))
-for s in deck.cards:
-    print ("%dx %s" % (s[1], format_card(get_card(s[0]))))
-print ("%s \nDeckstring: %s:" % (lb, deckstring))
-
+for i in sort_cards():
+    print ("%dx (%s) %s" % (i['ammount'], i['cost'], i['name']))
+print ("%s\nDeckstring: %s" % (lb, deckstring))
