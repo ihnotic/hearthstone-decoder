21b200848c95f03427f773cf80be9fcc1cced173
John Ihnotic <jihnotic@knuedge.com>
2017-12-08 12:54:31 -0800

    Added handling for unpadded base64 deckstrings

--- hs-decoder.py
+++ hs-decoder.py
@@ -3,16 +3,34 @@ import os
 import sys
 import json
 import requests
+import base64
 import argparse
 from hearthstone.deckstrings import Deck
 
+def decode_base64(data):
+    missing_padding = len(data) % 4
+    if missing_padding != 0:
+        data += b'='* (4 - missing_padding)
+    return data
+def get_format():
+    if int(deck.format) == 2:return "STANDARD"
+    else:return "WILD"
+def get_card(dbf_id):
+    return next((card for card in db if card["dbfId"] == dbf_id), None)
+def sort_cards():
+    cards = []
+    for s in deck.cards:
+        card = get_card(s[0])
+        card["ammount"] = int(s[1])
+        cards.append(card)
+    return sorted(cards, key=lambda card:(card["cost"], card["cardClass"], card["name"]))
 parser = argparse.ArgumentParser(description="Print hearthstone decklist to terminal from deckid")
 parser.add_argument('<deckid>', type=str, help='Hearthstone deckID to be converted')
 args = parser.parse_args()
 
 db_url = "https://api.hearthstonejson.com/v1/22611/enUS/cards.collectible.json"
 db_path = "./db.json"
-deckstring = sys.argv[1]
+deckstring = decode_base64(sys.argv[1])
 deck = Deck.from_deckstring(deckstring)
 lb = "="*30
 if os.path.exists(db_path):
@@ -27,18 +45,6 @@ else:
     else:
         raise RuntimeError("Couldn't download cards database: %s"
                            % response.text)
-def get_format():
-    if int(deck.format) == 2:return "STANDARD"
-    else:return "WILD"
-def get_card(dbf_id):
-    return next((card for card in db if card["dbfId"] == dbf_id), None)
-def sort_cards():
-    cards = []
-    for s in deck.cards:
-        card = get_card(s[0])
-        card["ammount"] = int(s[1])
-        cards.append(card)
-    return sorted(cards, key=lambda card:(card["cost"], card["cardClass"], card["name"]))
 print (get_card(deck.heroes[0])["cardClass"])
 print (get_format())
 print (lb)
